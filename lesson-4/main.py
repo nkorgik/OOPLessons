83dced54d2864be997bdece4cb0237a3b2db28d5
nkorgik <nkorgik639@gmail.com>
2022-07-13 17:20:48 +0300

    added lesson-4

---
+++ lesson-4/main.py
@@ -0,0 +1,14 @@
+# # 5, 17.8, 'cat', [1,2,3]
+#
+# print(isinstance(5, str))
+# print(isinstance(17.8, str))
+# print(isinstance('cat', str))
+# print(isinstance([1,2,3], str))
+
+
+class Person:
+    age = 16
+    name = 'Max'
+
+
+print(Person.__dict__)
