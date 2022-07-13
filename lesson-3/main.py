5ac63fc65a5e5a224a23e2b47ecbd798be1a052d
nkorgik <nkorgik639@gmail.com>
2022-07-13 15:58:33 +0300

    practice-3

---
+++ lesson-3/main.py
@@ -0,0 +1,21 @@
+class PersianCat:
+    breed = 'persian'  # attribute of this class
+    age = 12.0   # float
+
+
+class SiberianCat:
+    breed = 'siberian'
+    age = 0.5   # float
+
+
+class BengalCat:
+    breed = 'bengal'
+    age = 2.0  # float
+
+
+tom = PersianCat()
+garfield = SiberianCat()
+maxim = BengalCat()
+
+# print(type(tom), type(garfield), type(maxim), sep='\n')
+print(isinstance(5, int))
